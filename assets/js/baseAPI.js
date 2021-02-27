31bbe2ce5953f818638afe3f48ffacfc9bfc31c2
yanchen <891725371@qq.com>
2021-02-27 18:07:45 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,25 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options){
-    options.url='http://ajax.frontend.itheima.net'+options.url
-
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    // 为每个有权限的接口指定请求头
+    if (options.url.indexOf("/my/" !== -1)) {
+        options.headers = {
+            Authorization: localStorage.getItem('token' || '')
+        }
+    }
+    // 为每个有权限的接口挂在comolete函数
+    options.complete = res => {
+        console.log('执行了complate');
+        // console.log(res);
+        // res.responseJSON拿到服务器响应回来的数据
+        console.log(res.responseJSON);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1、强制清空token
+            localStorage.removeItem('token')
+            // 2、强制到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+$(function () {
+    // alert('11111')
+    //调用用户信息的函数
+    getUserinfo()
+
+    // 退出功能的实现
+    $('#btnExit').on('click', function () {
+        // 提示用户是否退出
+        var layer = layui.layer
+        layer.confirm('确定要退出登录吗?', { icon: 3, title: '提示' }, function (index) {
+            // 退出后的回调函数
+            //do something
+            // 1、清空本地存储的token
+            localStorage.removeItem('token')
+            // 2、重新跳转到登录页
+            location.href('/login.html')
+            // 关闭询问框
+            layer.close(index);
+        });
+    })
+
+
+})
+// 获取用户的基本信息
+function getUserinfo() {
+    //发起ajax请求获取用户的基本信息
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: res => {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            //渲染用户的基本信息 
+            renderAvatar(res.data)
+        },
+    })
+}
+// 渲染用户的基本信息
+function renderAvatar(userinfo) {
+    // 渲染用户的名称
+    var username = userinfo.username || userinfo.nickname
+    $('#welcome').html('欢迎&nbsp,&nbsp' + username)
+    // 渲染用户的头像
+    if (userinfo.user_pic !== null) {
+        $('.layui-nav-img')
+            .attr('src', userinfo.user_pic)
+            .show();
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        // 获取用户名的第一个字
+        var frist = username[0]
+        $('.text-avatar').html(frist).show()
+    }
+}
