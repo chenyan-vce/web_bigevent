df9f0cce62f53b827d9e21077301555cf326b807
yanchen <891725371@qq.com>
2021-02-27 10:22:26 +0800

    完成了注册和登录功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,4 @@
 $(function () {
-    // alert('11111')
     //登录和注册页面的切换
     $('#goreg').on('click', function () {
         $('.login-box').hide();
@@ -17,7 +16,6 @@ $(function () {
     form.verify({
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repwd: function (value) {
-            //通过形参拿到确认密码的值
             // 比较两次密码的值是否一致
             var pwd = $('.reg-box #pwd').val();
             if (pwd !== value) {
@@ -29,9 +27,7 @@ $(function () {
 
         // 监听注册表单的提交事件
         $('#form_reg').on('submit', function (e) {
-            // 1. 阻止默认的提交行为
             e.preventDefault()
-            // 2. 发起Ajax的POST请求
             var data = {
                 username: $('.reg-box #username').val(),
                 password: $('.reg-box #pwd').val()
@@ -43,7 +39,7 @@ $(function () {
                     return layer.msg(res.message)
                 }
                 layer.message('您已注册成功！请登录')
-                // 模拟人的点击行为 跳转登录页面
+                // 跳转登录页面
                 $('#goLogin').click()
             })
         }),
@@ -67,7 +63,7 @@ $(function () {
                     // 将登录成功得到的 token 字符串，保存到 localStorage 中
                     localStorage.setItem('token', res.token)
                     // 跳转到后台主页
-                    // location.href = '/index.html'
+                    location.href = '/index.html'
                 }
             })
         })
